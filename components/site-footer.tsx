5ddfb0852430c972dcf12e129a3efe1e366701a7
SONG <thdtjdgml415@naver.com>
2024-04-24 14:30:06 +0900

    url fix

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -7,15 +7,11 @@ export function SiteFooter() {
     <footer>
       <div className="mb-6 mt-14 flex flex-col items-center">
         <div className="mb-3 flex  space-x-4">
-          <a
-            href="mailto:thdtjdgml415@gmail.com"
-            target="_blank"
-            rel="noreforrer"
-          >
+          <a href="mailto:hello@example.com" target="_blank" rel="noreferrer">
             <span className="sr-only">Mail</span>
             <Mail className="h-6 w-6 " />
           </a>
-          <a href={siteConfig.links.github} target="_blank" rel="noreforrer">
+          <a href={siteConfig.links.github} target="_blank" rel="noreferrer">
             <span className="sr-only">GitHub</span>
             <Icons.gitHub className="h-6 w-6 " />
           </a>
