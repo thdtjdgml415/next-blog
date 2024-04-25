f8b9ff9baecf46d6eaac927346b4b5215ccebf60
SONG <thdtjdgml415@naver.com>
2024-04-25 16:52:12 +0900

    [feat] Add tags card component

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,9 @@
 import { posts } from "#site/content";
 import { PostItem } from "@/components/post-items";
 import { QueryPagination } from "@/components/query-pagination";
-import { sortPosts } from "@/lib/utils";
+import { Tag } from "@/components/tag";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { getAllTags, sortPosts, sortTagsByCount } from "@/lib/utils";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -25,6 +27,8 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
     POSTS_PER_PAGE * (currentPage - 1),
     POSTS_PER_PAGE * currentPage
   );
+  const tags = getAllTags(posts);
+  const sortedTags = sortTagsByCount(tags);
 
   return (
     <div className="container max-w-4xl py-6 lg:py-10">
@@ -34,29 +38,47 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
           <p className="text-xl text-muted-foreground">sdfkdls;fksd;fs</p>
         </div>
       </div>
-      <hr className="mt-8" />
-      {displayPosts?.length > 0 ? (
-        <ul className="flex flex-col">
-          {displayPosts.map((post) => {
-            const { slug, title, description, date, thumbnail, tags } = post;
-            return (
-              <li key={slug}>
-                <PostItem
-                  slug={slug}
-                  title={title}
-                  description={description}
-                  date={date}
-                  thumbnail={thumbnail.local}
-                  tags={tags}
-                />
-              </li>
-            );
-          })}
-        </ul>
-      ) : (
-        <p>nothing</p>
-      )}
-      <QueryPagination totalPages={totalPages} className="justify-end mt-4" />
+      <div className="grid grid-cols-12 gap-3 mt-8">
+        <div className="col-span-12 col-start-1 sm:col-span-8">
+          <hr className="mt-8" />
+          {displayPosts?.length > 0 ? (
+            <ul className="flex flex-col">
+              {displayPosts.map((post) => {
+                const { slug, title, description, date, thumbnail, tags } =
+                  post;
+                return (
+                  <li key={slug}>
+                    <PostItem
+                      slug={slug}
+                      title={title}
+                      description={description}
+                      date={date}
+                      thumbnail={thumbnail.local}
+                      tags={tags}
+                    />
+                  </li>
+                );
+              })}
+            </ul>
+          ) : (
+            <p>nothing</p>
+          )}
+          <QueryPagination
+            totalPages={totalPages}
+            className="justify-end mt-4"
+          />
+        </div>
+        <Card className="col-span-12 row-start-3 h-fit sm:col-span-4 sm:col-start-9 sm:row-start-1">
+          <CardHeader>
+            <CardTitle>Tags</CardTitle>
+          </CardHeader>
+          <CardContent className="flex flex-wrap gap-2">
+            {sortedTags?.map((tag) => (
+              <Tag tag={tag} key={tag} count={tags[tag]} />
+            ))}
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
